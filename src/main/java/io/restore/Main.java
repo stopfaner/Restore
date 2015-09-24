b5f724c2f3fd9222437092ed0d743223c9e1cb52
stopfaner <stopfan4er@gmail.com>
2015-09-25 00:07:06 +0300

    Added main dependencies and modified configuration file

--- src/main/java/io/restore/Main.java
+++ src/main/java/io/restore/Main.java
@@ -4,6 +4,9 @@ import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
+import javax.ws.rs.core.Application;
+import javax.ws.rs.ApplicationPath;
+
 import java.io.IOException;
 import java.net.URI;
 
@@ -11,9 +14,10 @@ import java.net.URI;
  * Main class.
  *
  */
-public class Main {
+@ApplicationPath("/api")
+public class Main extends Application{
     // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://localhost:8080/myapp/";
+    public static final String BASE_URI = "http://localhost:8080/";
 
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
